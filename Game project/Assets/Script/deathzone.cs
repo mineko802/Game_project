95ea37e9c6ca376907b1756ce11b50537425a76d
sato0320 <TTC2550SG0029@edu.tech.ac.jp>
2025-07-10 12:11:32 +0900

    敵モブへのダメージ付与

---
+++ Game project/Assets/Script/deathzone.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// ステージギミック：ダメージ床
+/// ブロックに乗ったアクター(またはトリガーに進入したアクター)にダメージを与える
+/// </summary>
+public class Gimmic_DamageBlock : MonoBehaviour
+{
+    // 設定項目
+    [Header("ダメージ量")]
+    public int damage;
+
+    // アクター接触時処理(Trigger)
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        string tag = collision.gameObject.tag;
+
+        if (tag == "ari")
+        {
+            // アリにダメージを与える
+            collision.gameObject.GetComponent<ari>().Damaged(damage);
+        }
+    }
+    // アクター接触時処理(Collider)
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        string tag = collision.gameObject.tag;
+        Debug.Log(tag);
+        if (tag == "ari")
+        {
+            // アリにダメージを与える
+            collision.gameObject.GetComponent<ari>().Damaged(damage);
+            Debug.Log("アリにぶつかった");
+        }
+    }
+}
+
